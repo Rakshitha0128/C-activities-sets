f23bfe4ba8f6d318059cbff04b0b9bc84e38845d
Rakshitha0128 <141307764+Rakshitha0128@users.noreply.github.com>
2024-01-25 09:47:30 +0530

    committed 1/25/2024, 9:47:30 AM

--- set01/problem05.c
+++ set01/problem05.c
@@ -1,1 +1,39 @@
+#include<stdio.h>
+int input();
+void compare(int a,int b,int c, int largest);
+void output(int a,int b,int c,int largest);
+int main()
+{
+    int a,b,c,largest;
+    a=input();
+    b=input();
+    c=input();
+    compare(a,b,c,largest);
+    output(a,b,c,largest);
+    return 0;
+}
+int input()
+{
+    int x;
+    printf("Enter the value");
+    scanf("%d",&x);
+    return x;
+}
+void compare(int a,int b,int c, int largest)
+{
+    if (a >= b && a >= c) {
+        largest = a;
+        return largest;
+    } else if (b >= a && b >= c) {
+        largest = b;
+         return largest;
+    } else {
+        largest = c;
+        return largest;
+    }
 
+}
+void output(int a,int b,int c,int largest)
+{
+    printf("The largest of %d,%d and %d is %d",a,b,c,largest);
+}

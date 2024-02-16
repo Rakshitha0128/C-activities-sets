0a670fe418c1b858d772269a2b568cfef329732d
Rakshitha0128 <141307764+Rakshitha0128@users.noreply.github.com>
2024-02-16 21:25:44 +0530

    committed 2/16/2024, 9:25:44 PM

---
+++ set02/problem02.c
@@ -0,0 +1,19 @@
+//Write a program to find if a triangle is scalene
+#include <stdio.h>
+
+int input_side();
+int check_scalene(int a, int b, int c);
+void output(int a, int b, int c, int isscalene);
+
+int main() {
+    int side1, side2, side3;
+    int isScalene;
+    side1 = input_side();
+    side2 = input_side();
+    side3 = input_side();
+
+    isScalene = check_scalene(side1, side2, side3);
+    output(side1, side2, side3, isScalene);
+
+    return 0;
+}

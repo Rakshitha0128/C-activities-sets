08d4b54fe1158bd5f1a077b78b1715908dc4e09f
Rakshitha0128 <141307764+Rakshitha0128@users.noreply.github.com>
2024-02-16 21:20:47 +0530

    committed 2/16/2024, 9:20:47 PM

--- set02/problem01.c
+++ set02/problem01.c
@@ -14,5 +14,23 @@ void output(float base, float height, float area);
     return 0;
     
     }
+    void input(float *base, float *height)
+{
+    printf("Enter the base of the triangle: ");
+    scanf("%f", base);
+    printf("Enter the height of the triangle: ");
+    scanf("%f", height);
+}
+void find_area(float base, float height, float *area)
+ {
+    *area = 0.5 * base * height;
+}
+void output(float base, float height, float area) 
+{
+    printf("Base: %.2f\n", base);
+    printf("Height: %.2f\n", height);
+    printf("Area: %.2f\n", area);
+}
+
 
 

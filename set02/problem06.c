e9a4a365f3df72669312216cbb8193fff95b4f4f
Rakshitha0128 <141307764+Rakshitha0128@users.noreply.github.com>
2024-02-24 07:35:11 +0530

    committed 2/24/2024, 7:35:10 AM

--- set02/problem06.c
+++ set02/problem06.c
@@ -1,24 +1,37 @@
+// Write a program to reverse a string.
+
 #include <stdio.h>
-int input();
-int find_gcd(int a, int b);
-void output(int a, int b, int gcd);
+#include <string.h>
+
+void input_string(char *a)
+ {
+    printf("Enter a string: ");
+    scanf("%s", a);
+}
+
+
+void str_reverse(char *str, char *rev_str) 
+{
+    int length = strlen(str);
+    int i, j;
+    for (i = length - 1, j = 0; i >= 0; i--, j++)
+    {
+        rev_str[j] = str[i];
+    }
+    rev_str[j] = '\0'; 
+}
+void output(char *a, char *reverse_a) 
+{
+    printf("Original string: %s\n", a);
+    printf("Reversed string: %s\n", reverse_a);
+}
 
 int main() 
 {
-    int num1, num2, gcd;
-    num1 = input();
-    num2 = input();
-    gcd = find_gcd(num1, num2);
-    output(num1, num2, gcd);
-
+    char str[100];
+    char rev_str[100];
+    input_string(str);
+    str_reverse(str, rev_str);
+    output(str, rev_str);
     return 0;
 }
-
-int input()
- {
-    int num;
-    printf("Enter a number: ");
-    scanf("%d", &num);
-    return num;
-}
-
